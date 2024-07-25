d5a93b343b49d9724a0e4da713b94e650d52f0ef
Matheus Monteiro <matheusmonteiro.home@gmail.com>
2024-07-24 23:53:11 -0300

    fix: corrige problemas de inicializacao do script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into account(id, food_balance, meal_balance, cash_balance) values ('1', '100', '200', '300');
+
+insert into merchant(id, name, mcc) values ('1', 'UBER TRIP                   SAO PAULO BR', '');
+insert into merchant(id, name, mcc) values ('2', 'UBER EATS                   SAO PAULO BR', '5411');
+insert into merchant(id, name, mcc) values ('3', 'EXTRA          RIO DE JANEI BR', '5811');
+insert into merchant(id, name, mcc) values ('4', 'PICPAY*BILHETEUNICO           GOIANIA BR', '');
--- src/main/resources/init.sql
+++
@@ -1,6 +0,0 @@
-insert into account values ('100', '200', '300');
-
-insert into merchant values ('UBER TRIP                   SAO PAULO BR', '',)
-insert into merchant values ('UBER EATS                   SAO PAULO BR', '5411')
-insert into merchant values ('EXTRA          RIO DE JANEI BR', '5811')
-insert into merchant values ('PICPAY*BILHETEUNICO           GOIANIA BR', '')
