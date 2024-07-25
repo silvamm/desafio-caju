36db68b38a4c0004141373fdfc92f98a46eca9ed
Matheus Monteiro <matheusmonteiro.home@gmail.com>
2024-07-24 23:56:56 -0300

    refactor: aumenta valores inicias da conta no script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into account(id, food_balance, meal_balance, cash_balance) values ('1', '100', '200', '300');
+insert into account(id, food_balance, meal_balance, cash_balance) values ('1', '300', '300', '300');
 
 insert into merchant(id, name, mcc) values ('1', 'UBER TRIP                   SAO PAULO BR', '');
 insert into merchant(id, name, mcc) values ('2', 'UBER EATS                   SAO PAULO BR', '5411');
